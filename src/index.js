9209d2a6a2bebfb1041631a588dfbf528da08499
The Mead <jasmead@gmail.com>
2016-11-09 14:22:23 -0600

    feat: Initial setup and code migration (#4)
    * feat: Initial setup and code migration
    
    fix: Add pre-commit package
    
    Closes #1
    
    * fix: Add npm credentials

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const BasicScheme = require('./auth/basic')
+
+exports.register = (server, options, next) => {
+  server.auth.scheme(BasicScheme.name, BasicScheme.implementation)
+  next()
+}
